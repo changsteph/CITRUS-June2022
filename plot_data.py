786eedcf691fb7d66e606965136ddd0623c16149
unknown <chang.m.steph@gmail.com>
2022-09-17 21:20:56 -1000

    Consolidated graphing functions

--- plot_data.py
+++ plot_data.py
@@ -1,52 +1,135 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-wild_file = "SelectedWindCount.csv"
-island_file = "Coastline.shp"
-temp_file = "Max_Temperatures.csv"
 
-# Selected stations (can remove)
-skn_used = [703, 840, 841.16, 901.2]
-wind_skn = [703, 840, 842.8, 911.1]
-skn_used.sort()
+def box_plot(data):
+    colors = ['magenta', 'green', 'blue', 'purple']
+    for i, w_value in enumerate(all_temp_dicts):
+        fig, ax = plt.subplots()
+        bp = ax.boxplot(w_value.values(), patch_artist=True)
 
-# Get data frame from csv files
-wind_table = pd.read_csv(wild_file, index_col=0)
-temp_table = pd.read_csv(temp_file, index_col=0)
-# latitude = list(wind_table["LAT"].loc[wind_table["ELEV.m."] <= 50])
-# longitude = list(wind_table["LON"].loc[wind_table["ELEV.m."] <= 50])
-# latitude = list(wind_table.loc[wind_skn]["LAT"])
-# longitude = list(wind_table.loc[wind_skn]["LON"])
-# t_latitude = list(temp_table.loc[skn_used]["LAT"])
-# t_longitude = list(temp_table.loc[skn_used]["LON"])
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(bp[element], color='black')
 
-skn_used = [39, 47, 324.6, 328.4, 339.4]
-lat = [19.5362, 19.47555556, 20.75788889, 20.75763889, 20.75980556]
-lon = [-155.5763, -155.3627778, -156.3200278, -156.2775, -156.2481667]
-# skn_list = list(temp_table.loc[temp_table['ELEV.m.'] <= 50].index)
-# print(t_latitude)
-# print(t_longitude)
+    for patch in bp['boxes']:
+        patch.set(facecolor=colors[i], alpha=0.5)
 
-hawaiian_islands = gpd.read_file(island_file)
-graph = hawaiian_islands.plot(color="Magenta")
-plt.scatter(x=lon, y=lat, color='Blue', s=4, label='Wind Stations')
-# for i in range(len(skn_used)):
-#     plt.annotate(str(skn_used[i]), xy=(lon[i] + 0.02, lat[i] + 0.02), textcoords='data')
-#
-# plt.scatter(x=t_longitude, y=t_latitude, color='Magenta', s=20, alpha=0.5, label='Temperature Stations')
-# for i in range(len(skn_used)):
-#     plt.annotate(str(skn_used[i]), xy=(t_longitude[i] + 0.01, t_latitude[i] - 0.01), textcoords='data')
+    ax.set_ylim(top=18, bottom=30)
+    # ax.set_xlabel("Months")
+    ax.set_ylabel("Temperature (C)")
+    ax.set_xticklabels(w_value.keys(), rotation=45)
+    ax.set_title(temp_station_names[i])
+
+    plt.plot(temp_mean_list[i], color='black', marker="o",
+             linestyle="dashed", markersize=3, alpha=0.5)
+    # for j, value in enumerate(temp_mean_list[i]):
+    #     plt.annotate(str(round(value, 3)), (j, value + 0.05))
+
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    plt.ylim(bottom=18, top=30)
+    plt.xlabel("Months", fontsize=15)
+    # plt.ylabel("Temperature (C)", fontsize=15)
+    # plt.title("Mean Oahu Temperatures", fontsize=25)
+    # plt.legend(["703 HNL", "840 Kaneohe Bay", "841.16 Camp Erdman", "901.1 Kii"], title="Stations", loc='upper left')
+
+    plt.savefig(temp_station_names[i] + "LinePlot.png", bbox_inches="tight")
+    plt.clf()
+    # file1 = open("TemperatureBoxPlotData.txt", "w")
+    # for j in range(len(all_temp_dicts)):
+    #     file1.write(temp_station_names[j] + " Box Plot Data\n")
+    #     for m in range(len(months_names)):
+    #         file1.write(months_names[m] + ":\n")
+    #         bp = plt.boxplot(all_temp_dicts[j][months_names[m]])
+    #         file1.write("Whiskers:\n")
+    #         for i in range(len(bp['whiskers'])):
+    #             median = bp["whiskers"][i].get_ydata()
+    #             file1.writelines(str(median) + "\n")
+    #
+    #         file1.write("Caps:\n")
+    #         for i in range(len(bp['caps'])):
+    #             median = bp["caps"][i].get_ydata()
+    #             file1.writelines(str(median) + "\n")
+    #
+    #         file1.write("Median:\n")
+    #         for i in range(len(bp['medians'])):
+    #             median = bp["medians"][i].get_ydata()
+    #             file1.writelines(str(median) + "\n")
+    #
+    #         file1.write("Fliers:\n")
+    #         for i in range(len(bp['fliers'])):
+    #             median = bp["fliers"][i].get_ydata()
+    #             file1.writelines(str(median) + "\n\n")
+    #     file1.write("\n\n")
+    #     plt.clf()
+    #
+    # file1.close()
 
 
-# for xy in zip(t_longitude, t_latitude):
-#     plt.annotate('(%s, %s)' % xy, xy=xy, textcoords='data')
-# for i in range(len(skn_list)):
-#     plt.annotate('%s' % skn_list[i], xy=(t_longitude[i], t_latitude[i]), textcoords='data')
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-# plt.legend(loc='lower left', title='Islands')
-plt.title("Map of Wind Speed Stations Used for Analysis", fontsize=12)
-plt.show()
+def line_plot(data, plot_name: str, x_axis: str, y_axis: str, line_color="black", point_type="o",
+              line_style="dashed", size=10):
+    months_names = ["January", "February", "March", "April", "May", "June", "July", "August",
+                    "September", "October", "November", "December"]
+
+    plt.plot(data, color=line_color, marker=point_type, linestyle=line_style, markersize=size)
+    for i, value in enumerate(data):
+        plt.annotate(str(round(value, 3)), (i, value + 0.05))
+
+    min_val = min(data) - 2
+    max_val = max(data) + 2
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], months_names)
+    plt.ylim(bottom=min_val, top=max_val)
+    plt.xlabel(x_axis, fontsize=15)
+    plt.ylabel(y_axis, fontsize=15)
+    plt.title(plot_name, fontsize=25)
+    # plt.legend(["legend1", "legend2", "legend3", "legend4"], title="Stations", loc='upper left')
+    plt.grid(True)
+    # plt.savefig("MeanWindSpeeds.png")
+    plt.show()
 
 
+def histogram(table, data, title_name, bin_width, bin_min, bin_max):
+    # a histogram returns 3 objects : n (i.e. frequencies), bins, patches
+    freq, bins, patches = plt.hist(data, edgecolor='white', label='d',
+                                   bins=np.arange(bin_min, bin_max, bin_width))
+
+    plt.xticks(np.arange(bin_min, bin_max, bin_width * 2))
+
+    # x coordinate for labels
+    bin_centers = np.diff(bins) * 0.5 + bins[:-1]
+
+    height_list = []
+    n = 0
+    for fr, x, patch in zip(freq, bin_centers, patches):
+        height = int(freq[n])
+        height_list.append(height)
+        if height > 0:
+            plt.annotate("{}".format(height),
+                         xy=(x, height),  # top left corner of the histogram bar
+                         xytext=(0, 0.2),  # offsetting label position above its bar
+                         textcoords="offset points",  # Offset (in points) from the *xy* value
+                         ha='center', va='bottom'
+                         )
+        n = n + 1
+
+    plt.yticks(range(max(height_list) + 1))
+    plt.title(title_name)
+    # plt.text(bin_min -0.1, max(height_list) - 1, text_string, fontsize=10, bbox=dict(facecolor='green', alpha=0.3),
+    #          ha='left', va='bottom')
+    plt.xlabel("Wind Speed m/s")
+    figure_name = title_name + ".png"
+    plt.savefig(figure_name)
+
+
+def scatter_with_image(x_column, y_column, annotation_column, shape_file, table_name, image_color="Orange",
+                       scatter_color="Blue", scatter_size=5, scatter_alpha=1.0):
+    image_file = gpd.read_file(shape_file)
+    graph = image_file.plot(color=image_color)
+    plt.scatter(x=x_column, y=y_column, color=scatter_color, s=scatter_size, alpha=scatter_alpha, label=table_name)
+    for i in range(len(annotation_column)):
+        plt.annotate(annotation_column[i], xy=(x_column[i] + 0.02, y_column[i] + 0.02), textcoords='data')
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    # plt.legend(loc='lower left', title='Islands')
+    plt.title(table_name, fontsize=12)
+    plt.show()
--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 from scipy.stats import shapiro
+from scipy.stats import mannwhitneyu
 
 # index = 2
 skn = 1023
@@ -64,3 +65,22 @@ for j in range(12):
 
 file2.close()
 # print(shapiro(month_list))
+
+# file1 = open("WindSpeedShapiro.txt", "w")
+#
+# for i in range(len(all_wind_dicts)):
+#     file1.write("\n" + wind_station_names[i] + " Shapiro-Wilk test results\n")
+#     for j in range(len(months_names)):
+#         results = shapiro(all_wind_dicts[i][months_names[j]])
+#         file1.write(months_names[j] + " " + str(results) + "\n")
+#
+# file1.close()
+
+# file1 = open("TemperatureMannWhitney.txt", "w")
+# file1.write(temp_station_names[2] + " + " + temp_station_names[3] + "\n")
+# for j in range(len(months_names)):
+#     results = mannwhitneyu(all_temp_dicts[2][months_names[j]], all_temp_dicts[3][months_names[j]])
+#     file1.write(months_names[j] + " : " + str(results) + "\n")
+#
+# file1.close()
+#
